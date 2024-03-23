d87e3a93903d9b8bb8e9494cdbf678b41c4d3089
magdalienkaa <magda.rodakova@gmail.com>
2024-03-23 15:36:33 +0100

    Update-23-3_2

--- database.js
+++ database.js
@@ -1,21 +1,21 @@
 require("dotenv").config();
 const { Client } = require("pg");
 
-// const client = new Client({
-//   host: "localhost",
-//   user: "postgres",
-//   port: 5432,
-//   password: "bc3bhi2201",
-//   database: "StudentAccommodationDB",
-// });
-
 const client = new Client({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  port: process.env.DB_PORT,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  host: "localhost",
+  user: "postgres",
+  port: 5432,
+  password: "bc3bhi2201",
+  database: "StudentAccommodationDB",
 });
+
+// const client = new Client({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   port: process.env.DB_PORT,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_DATABASE,
+// });
 
 client.connect();
 
