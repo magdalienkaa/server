bccbda19ecd50dd9469a0510d88dc3367970aebc
magdalienkaa <magda.rodakova@gmail.com>
2024-04-16 20:42:52 +0200

    import-pokus6

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const app = express();
 const routes = require("./routes");
 const cors = require("cors");
 
-const PORT = process.env.PORT || 5000;
+app.use(cors({ origin: "https://client-production-8f11.up.railway.app" }));
 app.use(express.json());
 
 app.use("/api", routes);
 
-app.use(cors({ origin: "https://client-production-8f11.up.railway.app" }));
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server beží na porte ${PORT}`);
