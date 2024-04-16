4be02066eae8db7bf70ce4a7e46104e163182b0c
magdalienkaa <magda.rodakova@gmail.com>
2024-04-16 21:06:29 +0200

    import-pokus8

--- server.js
+++ server.js
@@ -4,6 +4,23 @@ const routes = require("./routes");
 const cors = require("cors");
 
 app.use(cors({ origin: "https://client-production-8f11.up.railway.app" }));
+
+app.use(function (req, res, next) {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://client-production-8f11.up.railway.app"
+  );
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use(express.json());
 
 app.use("/api", routes);
