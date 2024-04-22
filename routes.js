3952ee9b6fce1e46fb7efdffb725b2ce13531449
magdalienkaa <magda.rodakova@gmail.com>
2024-04-22 15:59:51 +0200

    removed error word

--- routes.js
+++ routes.js
@@ -408,7 +408,7 @@ router.post("/uploadstudents", upload.single("myCSVFile"), (req, res) => {
       if (success) {
         res.status(200).send("File uploaded and processed successfully");
       } else {
-        console.log("Error uploading csv data", error);
+        console.log("Error uploading csv data");
         res.status(500).send("Error uploading csv data.");
       }
     });
